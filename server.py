bed47cc21dcf667674b39fe10f7c9f78f59a38e9
Jorgen Kvalvaag <jorgen.kvalvaag@nordicsemi.no>
2020-11-06 15:06:59 +0100

    Add udp sample test protocol

--- server.py
+++ server.py
@@ -1,15 +1,17 @@
 import asyncio
 import ssl
 
-PORT = 2442
-PORT_SSL = 2443
+BSDLIB_PORT = 2442
+BSDLIB_PORT_SSL = 2443
+UDP_SAMPLE_DUT_PORT = 5667
+UDP_SAMPLE_TEST_PORT = 5668
 
 counter = 0
-
+data_q = asyncio.Queue()
 ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 ssl_context.load_cert_chain(certfile='server.crt', keyfile='server.key')
 
-class DiscoveryProtocol(asyncio.DatagramProtocol):
+class BSDLibTestProtocol(asyncio.DatagramProtocol):
     counter = 0
     def __init__(self):
         super().__init__()
@@ -20,8 +22,7 @@ class DiscoveryProtocol(asyncio.DatagramProtocol):
         self.counter += 1
         self.transport.sendto(b'PONG: '+data, addr)
 
-counter = 0
-async def handle_echo(reader, writer):
+async def bsdlib_test_protocol_tls(reader, writer):
     global counter
     data = await reader.read(2048)
     print((counter, data))
@@ -30,21 +31,56 @@ async def handle_echo(reader, writer):
     await writer.drain()
     writer.close()
 
+class UDPSampleDUTProtocol(asyncio.DatagramProtocol):
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        print("dut rx")
+        if b'\x00\x00\x00' in data:
+            loop = asyncio.get_event_loop()
+            loop.create_task(self.task_add(data))
+
+    async def task_add(self, data):
+        await data_q.put(data)
+
+async def udp_sample_test_protocol(reader, writer):
+    data = await reader.read(2048)
+    print("tcp")
+    to_send = bytearray()
+    if b'foobar' in data:
+        while True:
+            try:
+                to_send.extend(f"Data: {data_q.get_nowait()}\n".encode())
+            except asyncio.QueueEmpty:
+                break
+    if not to_send:
+        writer.write(b"none")
+    else:
+        writer.write(to_send)       
+    await writer.drain()
+    print("done")
 
 def start_server():
     loop = asyncio.get_event_loop()
-    ipv4_udp = loop.create_datagram_endpoint(DiscoveryProtocol,local_addr=('0.0.0.0',PORT))
-    ipv6_udp = loop.create_datagram_endpoint(DiscoveryProtocol,local_addr=('::',PORT))
-    ipv4_tcp = asyncio.start_server(handle_echo, '0.0.0.0', PORT, loop=loop)
-    ipv6_tcp = asyncio.start_server(handle_echo, '::', PORT, loop=loop)
-    ipv4_tcp_ssl = asyncio.start_server(handle_echo, '0.0.0.0', PORT_SSL, loop=loop, ssl=ssl_context)
-    ipv6_tcp_ssl = asyncio.start_server(handle_echo, '::', PORT_SSL, loop=loop, ssl=ssl_context)
-    loop.run_until_complete(ipv4_tcp)
-    loop.run_until_complete(ipv4_udp)
-    loop.run_until_complete(ipv6_tcp)
-    loop.run_until_complete(ipv6_udp)
-    loop.run_until_complete(ipv4_tcp_ssl)
-    loop.run_until_complete(ipv6_tcp_ssl)
+    bsdlib_ipv4_udp = loop.create_datagram_endpoint(BSDLibTestProtocol,local_addr=('0.0.0.0',BSDLIB_PORT))
+    bsdlib_ipv6_udp = loop.create_datagram_endpoint(BSDLibTestProtocol,local_addr=('::',BSDLIB_PORT))
+    bsdlib_ipv4_tcp = asyncio.start_server(bsdlib_test_protocol_tls, '0.0.0.0', BSDLIB_PORT, loop=loop)
+    bsdlib_ipv6_tcp = asyncio.start_server(bsdlib_test_protocol_tls, '::', BSDLIB_PORT, loop=loop)
+    bsdlib_ipv4_tcp_ssl = asyncio.start_server(bsdlib_test_protocol_tls, '0.0.0.0', BSDLIB_PORT_SSL, loop=loop, ssl=ssl_context)
+    bsdlib_ipv6_tcp_ssl = asyncio.start_server(bsdlib_test_protocol_tls, '::', BSDLIB_PORT_SSL, loop=loop, ssl=ssl_context)
+    udpsample_dut = loop.create_datagram_endpoint(UDPSampleDUTProtocol,local_addr=('0.0.0.0',UDP_SAMPLE_DUT_PORT))
+    udpsample_test = asyncio.start_server(udp_sample_test_protocol, '0.0.0.0', UDP_SAMPLE_TEST_PORT, loop=loop)
+    
+    loop.run_until_complete(bsdlib_ipv4_tcp)
+    loop.run_until_complete(bsdlib_ipv4_udp)
+    loop.run_until_complete(bsdlib_ipv6_tcp)
+    loop.run_until_complete(bsdlib_ipv6_udp)
+    loop.run_until_complete(bsdlib_ipv4_tcp_ssl)
+    loop.run_until_complete(bsdlib_ipv6_tcp_ssl)
+    loop.run_until_complete(udpsample_dut)
+    loop.run_until_complete(udpsample_test)
+
     try:
         loop.run_forever()
     except KeyboardInterrupt:
